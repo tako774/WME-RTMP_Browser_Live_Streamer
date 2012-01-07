9b2480925b6d1f35812dd72a53e311f3b06d6536
nanashi <nanashi.hatch@gmail.com>
2012-01-07 20:04:47 +0900

    配信説明テキストに、// または # を行頭においてコメント行を書けるように
    配信説明テキストに、// または # を行頭においてコメント行を書けるように
    
    Signed-off-by: nanashi <nanashi.hatch@gmail.com>

--- js/bc.js
+++ js/bc.js
@@ -1,6 +1,16 @@
 // 配信説明をサーバーから読み込んで表示
 function load_description() {
-	$.get("config/description.txt", function (description) {
+	$.get("config/description.txt", function (desc_dat) {
+		var description = "";
+		var lines = desc_dat.split(/\r\n|\r|\n/);
+		
+		// 行頭が // または # であればコメント行扱いとし表示しない
+		$.each(lines, function() {
+			if (!this.match(/^(\/\/|\#)/)) {
+				description += this + "\n";
+			}
+		});
+		
 		$("#description").text(description);
 	});
 }
